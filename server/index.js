f7097be94a24fa4985f7fb0471c3be0752456306
Rick Schlimback <rickschlimback@MacBook-Pro-van-Rick.local>
2025-06-14 18:33:53 +0200

    Gebruik dynamische CORS origin-functie en voeg frontend Render URL toe aan allowed origins

--- server/index.js
+++ server/index.js
@@ -46,12 +46,20 @@ const checkDatabase = (req, res, next) => {
 const allowedOrigins = [
   'https://warmeleads-crm.onrender.com',
   'https://www.warmeleadscrm.nl',
+  'https://warmeleads-frontend.onrender.com',
   'http://localhost:3000'
 ];
 
 app.use(helmet());
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 
